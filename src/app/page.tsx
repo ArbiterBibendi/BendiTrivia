43632b25d785603f46a467da89061ade802cc1ca
Drake Scott <ArbiterBibendi@yahoo.com>
2024-07-30 12:11:36 -0500

    connect card to api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 import { Card } from "../components/card";
-export default function Home() {
+import { Trivia } from "../components/card";
+
+async function getTrivia(): Promise<Trivia> {
+  const url: string = process.env.HOST_URL as string;
+  const response: Response = await fetch(url + "/api/trivia", {
+    cache: "no-store",
+  });
+  if (!response.ok) {
+    throw new Error("Something went wrong fetching trivia");
+  }
+  const responseJson = await response.json();
+  const trivia: Trivia = responseJson.message;
+  return trivia;
+}
+export default async function Home() {
+  const trivia2 = await getTrivia();
   return (
     <>
       <h1>BendiTrivia</h1>
       <div className="cardHolder">
-        <Card 
-          question={"According to Arthurian legend, what did King Arthur receive as a dowry, on his marriage to Guinevire"}
-          answer={"The round table"}
-          category={"General"}
-        />
+        <Card trivia={trivia2} />
       </div>
     </>
   );
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,9 +1,15 @@
-export function Card({question, answer, category}: {question:string, answer:string, category:string}) {
-    return (
-        <div className="card">
-          <h3 className="category">{category}</h3>
-          <h2>{question}</h2>
-          <h2 className="answer">{answer}</h2>
-        </div>
-    )
+export type Trivia = {
+  question: string;
+  answer: string;
+  category: string;
+};
+export function Card({ trivia }: { trivia: Trivia }) {
+  const { question, answer, category } = trivia;
+  return (
+    <div className="card">
+      <h3 className="category">{category}</h3>
+      <h2>{question}</h2>
+      <h2 className="answer">{answer}</h2>
+    </div>
+  );
 }
