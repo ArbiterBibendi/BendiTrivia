fd1a319435a8513726707b0f949a475e30400aa7
Drake Scott <ArbiterBibendi@yahoo.com>
2024-07-29 20:25:35 -0500

    prototyping card page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1>BendiTrivia</h1>
+    <>
+      <h1>BendiTrivia</h1>
+      <div className="cardHolder">
+        <div className="card">
+          <h2>According to Arthurian legend, what did King Arthur receive as a dowry, on his marriage to Guinevire</h2>
+          <h2 className="answer">the round table</h2>
+        </div>
+      </div>
+    </>
   );
 }
