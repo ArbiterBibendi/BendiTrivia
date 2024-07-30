b6db19a691163bd1cb414507dd8764921a7da3b7
Drake Scott <ArbiterBibendi@yahoo.com>
2024-07-30 14:15:52 -0500

    pass id search param to api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Card } from "../components/card";
 import { Trivia } from "../components/card";
 
-async function getTrivia(): Promise<Trivia> {
-  const url: string = process.env.HOST_URL as string;
-  const response: Response = await fetch(url + "/api/trivia", {
-    cache: "no-store",
-  });
+const url: string = process.env.HOST_URL as string;
+async function getTrivia(id?: string): Promise<Trivia> {
+  const response: Response = await fetch(
+    url + "/api/trivia" + (id ? `?id=${id}` : ""),
+    {
+      cache: "no-store",
+    }
+  );
   if (!response.ok) {
     throw new Error("Something went wrong fetching trivia");
   }
@@ -14,13 +17,19 @@ async function getTrivia(): Promise<Trivia> {
   return trivia;
 }
 
-export default async function Home() {
-  const trivia2 = await getTrivia();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { id?: string; category?: string };
+}) {
+  const trivia = await getTrivia(searchParams["id"]);
+
+  console.log(searchParams);
   return (
     <>
       <h1>BendiTrivia</h1>
       <div className="cardHolder">
-        <Card trivia={trivia2} />
+        <Card trivia={trivia} />
       </div>
     </>
   );
