4ec3c404ca8e6bb3f47134de7b6f5d1acbc826fb
Drake Scott <ArbiterBibendi@yahoo.com>
2024-07-30 12:11:58 -0500

    prettier

--- src/app/api/trivia/[[...category]]/route.ts
+++ src/app/api/trivia/[[...category]]/route.ts
@@ -1,21 +1,25 @@
-import { QueryResult, sql } from '@vercel/postgres';
+import { QueryResult, sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { category: string } },
+) {
+  const category = params.category ? params.category[0] : null;
+  let trivia: QueryResult;
+  if (category == null || category.toLowerCase() == "all") {
+    trivia =
+      await sql`SELECT question, answer, category FROM trivia OFFSET floor(random() * (SELECT COUNT(*) FROM trivia)) LIMIT 1;`;
+  } else {
+    trivia =
+      await sql`SELECT question, answer, category FROM trivia WHERE category ILIKE ${category} OFFSET floor(random() * (SELECT COUNT(*) FROM trivia WHERE category ILIKE ${category})) LIMIT 1;`;
+  }
 
-export async function GET(request: NextRequest, { params }: { params: { category: string }}) {
-    const category = params.category ? params.category[0] : null;
-    let trivia:QueryResult;
-    if (category == null || category.toLowerCase() == 'all') {
-        trivia = await sql`SELECT question, answer, category FROM trivia OFFSET floor(random() * (SELECT COUNT(*) FROM trivia)) LIMIT 1;`;
-    } 
-    else {
-        trivia = await sql`SELECT question, answer, category FROM trivia WHERE category ILIKE ${category} OFFSET floor(random() * (SELECT COUNT(*) FROM trivia WHERE category ILIKE ${category})) LIMIT 1;`;
-    }
-    
-    
-    if (trivia.rows.length <= 0) {
-        return NextResponse.json({"message": "No question found with those parameters"});
-    }
-    return NextResponse.json({"message": trivia.rows});
+  if (trivia.rows.length <= 0) {
+    return NextResponse.json({
+      message: "No question found with those parameters",
+    });
+  }
+  return NextResponse.json({ message: trivia.rows[0] });
 }
 export const revalidate = 0;
