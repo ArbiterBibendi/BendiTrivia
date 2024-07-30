38d1f34c5bba7d1e77eac79d69d80432ad141ab4
Drake Scott <ArbiterBibendi@yahoo.com>
2024-07-30 13:11:27 -0500

    monday

--- src/app/api/trivia/route.ts
+++ src/app/api/trivia/route.ts
@@ -8,7 +8,8 @@ export async function GET(
   const searchParams = request.nextUrl.searchParams;
   const category: string | null = searchParams.get("category");
   const id: string | null = searchParams.get("id");
-  
+
+  // only use category if no id is specified
   let trivia: QueryResult;
   if (id != null) {
     trivia =
