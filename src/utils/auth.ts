e4114b1cfc49d37193490d918093be36a33bd3e7
Drake Scott <ArbiterBibendi@yahoo.com>
2024-09-02 12:28:21 -0500

    validateRequest

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,8 @@
 import { PostgresJsAdapter } from "@lucia-auth/adapter-postgresql";
 import { Lucia } from "lucia";
 import postgres from "postgres";
+import type { User, Session } from "lucia";
+import { cookies } from "next/headers";
 
 const sql = postgres();
 const adapter = new PostgresJsAdapter(sql, {
@@ -22,6 +24,37 @@ export const lucia = new Lucia(adapter, {
     };
   },
 });
+
+export async function validateRequest(user: User, session: Session) {
+  const sessionId = cookies().get(lucia.sessionCookieName);
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    };
+  }
+
+  const result = await lucia.validateSession(sessionId.value);
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+  } catch {}
+  return result;
+}
 
 interface DatabaseUserAttributes {
   username: string;
