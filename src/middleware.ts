83f9ebf0ba00e43a67b284803754983b2ab2ef7d
Drake Scott <ArbiterBibendi@yahoo.com>
2024-09-17 15:42:39 -0500

    dont rate limit deletes

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,11 @@ const ratelimit = new Ratelimit({
   limiter: Ratelimit.slidingWindow(5, "10 s"),
 });
 export default async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/api")) {
+  // request.method != "DELETE" because all DELETE routes are currently admin only and we arent worried about admins abusing rate limit
+  if (
+    request.nextUrl.pathname.startsWith("/api") &&
+    request.method != "DELETE"
+  ) {
     const { success, remaining } = await ratelimit.limit("api");
     if (!success) {
       return NextResponse.json({ error: "Rate limited" }, { status: 429 });
