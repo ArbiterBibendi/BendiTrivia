1246171e24a8b04a0568eccf4bfe7dcdf6792c31
Drake Scott <ArbiterBibendi@yahoo.com>
2025-06-16 15:08:33 -0500

    ok

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(nextRequest: NextRequest) {
 	try {
 		await sql`SELECT 1`;
-		return NextResponse.json({}, { status: 200});
+		return NextResponse.json({message: "OK"}, { status: 200});
 	} catch(e) {
 		return NextResponse.json({error: "There was an issue connecting to the database"}, { status: 500});
 	}
