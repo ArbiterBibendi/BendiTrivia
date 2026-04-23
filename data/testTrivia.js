c4b28b8a0bd3dbe7f2c0d3adbc999a9ad0e1a24a
Drake Scott <ArbiterBibendi@yahoo.com>
2026-04-22 20:34:40 -0500

    push data

---
+++ data/testTrivia.js
@@ -0,0 +1,6 @@
+const trivia = {
+  question:
+    "According to Arthurian legend, what did King Arthur receive as a dowry, on his marriage to Guinevire",
+  answer: "The round table",
+  category: "General",
+};
